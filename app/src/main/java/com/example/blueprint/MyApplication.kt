4f947188511b22d05f2db2bc756d227a5c34fd4c
EvilZ <zenangao@gmail.com>
2020-06-08 14:04:21 -0500

    [ZeNan] Setup initial app template along with commonly used dependencies

---
+++ app/src/main/java/com/example/blueprint/MyApplication.kt
@@ -0,0 +1,46 @@
+package com.example.blueprint
+
+import android.app.Application
+import android.os.StrictMode
+import android.os.StrictMode.VmPolicy
+import android.provider.Settings
+import android.provider.Settings.Global.DEVELOPMENT_SETTINGS_ENABLED
+import androidx.viewbinding.BuildConfig
+import timber.log.Timber
+import timber.log.Timber.DebugTree
+
+
+class MyApplication : Application() {
+    override fun onCreate() {
+        enabledStrictModeIfNeeded()
+        super.onCreate()
+        if (BuildConfig.DEBUG) Timber.plant(DebugTree())
+    }
+
+    private fun enabledStrictModeIfNeeded() {
+        if (!BuildConfig.DEBUG) return
+
+        val isDeveloperModeEnabled = Settings.Secure
+            .getInt(contentResolver, DEVELOPMENT_SETTINGS_ENABLED, 0) != 0
+
+        if (!isDeveloperModeEnabled) return
+
+        StrictMode.setThreadPolicy(
+            StrictMode.ThreadPolicy.Builder()
+                .detectDiskReads()
+                .detectDiskWrites()
+                .detectNetwork() // or .detectAll() for all detectable problems
+                .penaltyLog()
+                .penaltyDeath()
+                .build()
+        )
+        StrictMode.setVmPolicy(
+            VmPolicy.Builder()
+                .detectLeakedSqlLiteObjects()
+                .detectLeakedClosableObjects()
+                .penaltyLog()
+                .penaltyDeath()
+                .build()
+        )
+    }
+}
---
+++ app/src/main/java/com/example/blueprint/ui/home/HomeFragment.kt
@@ -0,0 +1,29 @@
+package com.example.blueprint.ui.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import com.example.blueprint.R
+
+class HomeFragment : Fragment() {
+
+    private val homeViewModel: HomeViewModel by viewModels<HomeViewModel>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val root = inflater.inflate(R.layout.fragment_home, container, false)
+        val textView: TextView = root.findViewById(R.id.text_home)
+        homeViewModel.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })
+        return root
+    }
+}
