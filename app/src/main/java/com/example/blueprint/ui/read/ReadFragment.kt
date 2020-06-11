0f55ccbc7c9770395dc03d166bbb1417ab02d4e4
EvilZ <zenangao@gmail.com>
2020-06-11 10:42:16 -0500

    [ZeNan] implement MVVM logic for read me screen and rename dimen

--- app/src/main/java/com/example/blueprint/ui/read/ReadFragment.kt
+++ app/src/main/java/com/example/blueprint/ui/read/ReadFragment.kt
@@ -5,11 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import com.example.blueprint.databinding.FragmentReadBinding
 
 class ReadFragment : Fragment() {
 
     private lateinit var binding: FragmentReadBinding
+    private val readViewModel by viewModels<ReadViewModel>()
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -20,6 +23,8 @@ class ReadFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-
+        readViewModel.message.observe(viewLifecycleOwner, Observer {
+            binding.messageText.text = it
+        })
     }
 }
--- app/src/main/java/com/example/blueprint/ui/read/ReadViewModel.kt
+++ app/src/main/java/com/example/blueprint/ui/read/ReadViewModel.kt
@@ -1,5 +1,77 @@
 package com.example.blueprint.ui.read
 
+import android.graphics.Color
+import android.text.SpannableStringBuilder
+import android.text.style.BulletSpan
+import androidx.core.text.bold
+import androidx.core.text.color
+import androidx.core.text.inSpans
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
-class ReadViewModel : ViewModel()
+class ReadViewModel : ViewModel() {
+    private val _message = MutableLiveData<CharSequence>()
+    val message: LiveData<CharSequence>
+        get() = _message
+
+    init {
+        viewModelScope.launch {
+            val info = withContext(Dispatchers.IO) {
+                buildReadMeInfo()
+            }
+            _message.value = info
+        }
+    }
+
+    private fun buildReadMeInfo(): CharSequence {
+        return SpannableStringBuilder().apply {
+            color(Color.BLUE) {
+                bold { append("Android Architecture BluePrint\n") }
+            }
+            append("ZeNan Gao | gzn3926@gmail.com\n")
+            append("https://github.com/zen3926/Android-Architecture-BluePrint.git\n\n")
+
+
+            append("This serves as a demo/blueprint for various Android components and Feature:\n")
+            buildContent()
+            append("\n")
+
+            append("Other 3rd-party dependent libraries used:\n")
+            buildAdditionalLibrariesUsed()
+        }
+    }
+
+    private fun SpannableStringBuilder.buildContent() {
+        val contents = listOf<String>(
+            "Android App Components: [Activity, Services, Broadcast Receivers]",
+            "Android Navigation Component: [Navigation Graph, Controller]",
+            "Android Architecture Components: [View Binding, Data Binding, LiveData, Room, Work Manager, ViewModel, SaveStateHandle]",
+            "View Components: [RecyclerView, CardView, ConstraintLayout, TextInputLayout, and many other Material View Components]",
+            "Additional Features: [App Widget, App Shortcut, Notification, Security Storage, Biometric Authentication]"
+        )
+
+        contents.forEach {
+            inSpans(BulletSpan(30, Color.BLUE)) { append(it) }
+            append("\n")
+        }
+    }
+
+    private fun SpannableStringBuilder.buildAdditionalLibrariesUsed() {
+        val contents = listOf<String>(
+            "Network: Retrofit 2",
+            "Json parsing: Moshi",
+            "Logging: Timber",
+            "Image Loading: Glide",
+            "Unit Testing: JUnit, Espresso, Truth, Mockk"
+        )
+        contents.forEach {
+            inSpans(BulletSpan(30, Color.BLUE)) { append(it) }
+            append("\n")
+        }
+    }
+}
